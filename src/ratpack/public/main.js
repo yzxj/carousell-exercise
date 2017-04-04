81f4421f0cac56f3a8b27847f60aacbe80993ec0
Justin Yeo <nitsuj.oey@gmail.com>
2017-04-04 22:53:41 +0800

    FE: Added some API callers. TODO: Complete when actual API is done.

--- src/ratpack/public/main.js
+++ src/ratpack/public/main.js
@@ -1,5 +1,46 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
-});
+
+app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.topics = [];
+
+    $scope.getTopics = function() {
+        $http({
+            method: 'GET',
+            url: '/api/topics'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.topics = response.data.topics;
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    };
+
+    $scope.upvote = function(topicContent, topicVotes, topicTimeCreated) {
+        $scope.vote('/api/upvote', topicContent, topicVotes, topicTimeCreated);
+    };
+
+    $scope.downvote = function(topicContent, topicVotes, topicTimeCreated) {
+        $scope.vote('/api/downvote', topicContent, topicVotes, topicTimeCreated);
+    };
+
+    $scope.vote = function(voteUrl, topicContent, topicVotes, topicTimeCreated) {
+        $http({
+            method: 'POST',
+            url: voteUrl,
+            data: {
+                content: topicContent,
+                upvotes: topicVotes,
+                timeCreated: topicTimeCreated
+            }
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            // TODO: Refresh page OR fetch list again OR the api returns new list
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+}]);
