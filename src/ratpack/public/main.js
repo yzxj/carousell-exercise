ddd7baa28b22c5054b9d3c0e5036309817f7d869
Justin Yeo <nitsuj.oey@gmail.com>
2017-04-04 23:35:12 +0800

    FE: homepage baseline functionality done. Added init() and sorting for topics. TODO: test if sorting is necessary.

--- src/ratpack/public/main.js
+++ src/ratpack/public/main.js
@@ -1,19 +1,26 @@
-var app = angular.module('myApp', []);
+var app = angular.module('didditApp', []);
 
-app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+app.controller('didditCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.topics = [];
+
+    $scope.init = function() {
+        $scope.getTopics();
+    };
 
     $scope.getTopics = function() {
         $http({
             method: 'GET',
             url: '/api/topics'
         }).then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            $scope.topics = response.data.topics;
+            // TODO: Check functionality.
+            console.log(response.data.topics);
+            $scope.topics = response.data.topics.sort(function(a,b){
+                if (a.upvotes == b.upvotes) {
+                    return b.timeCreated - a.timeCreated;
+                }
+                return b.upvotes - a.upvotes;
+            });
         }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
         });
     };
 
@@ -52,5 +59,8 @@ app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
         });
-    }
+    };
+
+    // Run Initialization
+    $scope.init();
 }]);
